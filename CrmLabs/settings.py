959d5a553233a62831fda0eba6bf27a84b097dd7
LapkinLab <lapppkin@yahoo.com>
2021-11-11 01:32:17 +0300

    Deploy

--- CrmLabs/settings.py
+++ CrmLabs/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-import dj-database-url
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -96,9 +96,6 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
-db_from_env = dj-database-url.config()
-DATABASE['default'].update(db_from_env)
 
 
 # Password validation
